c0888d9d0bee23d97ff33824e513ad382befe000
unknown <youeyheang61@gmail.com>
2026-01-13 05:53:27 -0800

    add helloworld message

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main (void){
+  char name [30];
+  int score;
+  printf("Dara:");
+  scanf("%3s",name);
+  printf("Dara");
+  scanf("%d",&score);
+  printf("\n---student info---\n");
+  printf("name=%s\n",name);
+  printf("score=%d\n",score);
+  
+  
+ return 0;
+}
