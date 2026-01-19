4c1e33b42109e932fdab357ca171bd441f70689d
unknown <youeyheang61@gmail.com>
2026-01-19 20:19:59 +0700

    add helloworld message

---
+++ chp_2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int length = 4;
+    int width = 6;
+    int area; 
+    area = length * width;
+
+    printf("area = %d\n" ,area);
+    
+    return 0;
+}
--- main.c
+++ main.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 
 int main() {
-  printf("Hello World!");
-  printf("Hello World!");
+  
+  // vairable 
+  char name[30];
+  int score;
+  
+  // input 
+  printf("Enter student name: "); // show message nov terminal 
+  scanf("%29s", name); // input name var 
+  // ban dara name
+  printf("Enter Socre:"); 
+  scanf("%d", &score);
+
+  // ban score 85
+  // out put 
+
+  // jea messge 
+  printf("/n--- Student Info ---\n");
+  printf("Name = %s\n" , name);
+  printf("Score = %d\n" ,score);
+  
   return 0;
+
 }
